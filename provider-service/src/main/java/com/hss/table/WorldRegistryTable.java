eea9ff5ab5c23e9645ecba892689207bbee0659a
‘HeTreeTerr’ <3110708879@qq.com>
2021-09-15 01:09:00 +0800

    世界注册信息表，高并发优化

--- provider-service/src/main/java/com/hss/table/WorldRegistryTable.java
+++ provider-service/src/main/java/com/hss/table/WorldRegistryTable.java
@@ -5,14 +5,19 @@ import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Vector;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
  * 世界注册信息表
  */
 @Component
 public class WorldRegistryTable {
-
-    private List<World> worldList = new Vector<>();
+    /*
+    高并发优化
+    volatile 保证可见性、禁止指令重排
+    CopyOnWriteArrayList  保证高并发安全
+     */
+    private volatile List<World> worldList = new CopyOnWriteArrayList<>();
 
     public List<World> getWorldList() {
         return worldList;
